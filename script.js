3d00f54be677b405d7cecfa9037d0f0d1c7bf31c
AdityaChandra24 <99462111+AdityaChandra24@users.noreply.github.com>
2022-11-27 12:25:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ setInterval(()=>{
     counter.innerText = count;
     count++
     
-    if(count > 5) location.replace('https://adityachandraa.notion.site/Projects-e1577b3170734a969a73e7f1e961404b')
+    if(count > 5) location.replace('https://adityachandra.notion.site/Aditya-Chandra-727eb31f4d54434493c0998bc06a23a0')
     
 },1000)
