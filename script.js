bbb455d174baf0d124e145d8ec16a8ff025cdd96
AdityaChandra24 <99462111+AdityaChandra24@users.noreply.github.com>
2022-11-27 12:27:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-let counter = document.querySelector('h1');
-let count = 1;
-
-setInterval(()=>{
-
-    counter.innerText = count;
-    count++
-    
-    if(count > 5) location.replace('https://adityachandra.notion.site/Aditya-Chandra-727eb31f4d54434493c0998bc06a23a0')
-    
-},1000)
+location.replace('https://adityachandra.notion.site/Aditya-Chandra-727eb31f4d54434493c0998bc06a23a0')
