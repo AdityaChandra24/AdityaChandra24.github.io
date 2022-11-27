ecb09046b9cff05d6b650ef4fe8d3ebb647a8855
AdityaChandra24 <99462111+AdityaChandra24@users.noreply.github.com>
2022-11-27 21:23:27 +0530

    Update password.js

--- password.js
+++ password.js
@@ -1,7 +1,7 @@
 function passwordChecker(){
     var enteredPassword = document.getElementById("ipass").value;
     if(enteredPassword=="AMS-C-A777"){
-        window.location.href = "https://www.notion.so/adityachandra/Private-7ffb6e9852d14a5b9821519152a4496a";
+        window.location.href = "https://adityachandraa.notion.site/Private-9bd89deeea7047d591397cd68f20d98d";
         alert("Redirecting");
     }
     else{
