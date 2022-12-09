8b60f7943670b6f29becc6ce52484cb5bfe1f68f
AdityaChandra24 <99462111+AdityaChandra24@users.noreply.github.com>
2022-12-09 15:31:14 +0530

    Update password.js

--- password.js
+++ password.js
@@ -2,7 +2,7 @@ function passwordChecker(){
     var enteredPassword = document.getElementById("ipass").value;
     if(enteredPassword=="happybirthday"){
         window.location.href = "https://adityachandraa.notion.site/Private-9bd89deeea7047d591397cd68f20d98d";
-        setTimeout(passwordChecker, 200)
+        setTimeout(passwordChecker, 2000)
        
     }
     else{
